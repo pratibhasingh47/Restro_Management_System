3314de0171a3c39a7d18073628d4add77460c4af
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-11-03 22:26:09 +0530

    update

--- backend/middleware/globarErrorhandlers.js
+++ backend/middleware/globarErrorhandlers.js
@@ -14,7 +14,6 @@
 // module.exports = errorHandler;
 
 
-
 const errorHandler = (err, req, res, next) => {
     let statusCode = err.statusCode || 500;
     let message = err.message || "Internal Server Error";
