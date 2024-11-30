313b241e4e44aaa2e02e2c0f04417944dea554da
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-12-01 02:25:30 +0530

    enhance UI components

--- Frontend/src/redux/reducers/staffPersonal.ts
+++ Frontend/src/redux/reducers/staffPersonal.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
 interface StaffPersonalState {
     staffPersonal: any;
