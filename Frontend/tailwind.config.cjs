6f230999d2bda7439a02a8a36dd07d67b449ab79
pratibhasingh47 <pratibhasingh1624@gmail.com>
2025-01-25 21:30:36 +0530

    formatting

--- Frontend/tailwind.config.cjs
+++ Frontend/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust the paths to match your project structure
+    "./src/**/*.{js,jsx,ts,tsx}",  // Adjust the paths to match your project structure
   ],
   theme: {
     extend: {
