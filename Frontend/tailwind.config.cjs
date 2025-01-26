ef950caf44dae7b2193db77cbf2c5fc9c5635f14
pratibhasingh47 <pratibhasingh1624@gmail.com>
2025-01-26 21:00:35 +0530

    formatting

--- Frontend/tailwind.config.cjs
+++ Frontend/tailwind.config.cjs
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // Adjust the paths to match your project structure
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        lato: ['Lato', 'sans-serif'],
-      },
-      colors: {
-        primary: '#001D3D',
-        secondary: '#003566',
-        accent1: '#FFC300',
-        accent2: '#FFD60A',
-        dark: '#000814',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",  // Adjust the paths to match your project structure
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				lato: ['Lato', 'sans-serif'],
+			},
+			colors: {
+				primary: '#001D3D',
+				secondary: '#003566',
+				accent1: '#FFC300',
+				accent2: '#FFD60A',
+				dark: '#000814',
+			},
+		},
+	},
+	plugins: [],
 };
